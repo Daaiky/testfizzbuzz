681efd92f7f4cff99b47f06a909e97a6611717fc
Daiki <daiki.@DaikinoAir.local>
2020-02-29 23:12:58 +0900

    add new file

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const hoge = document.getElementById('btn');
-hoge.addEventListener('click',()=>{
+const btn = document.getElementById('btn');
+btn.addEventListener('click' , () => {
 
-    const li = document.createElement('li');
     const fizz = document.getElementById('Fizz').value;
     const buzz = document.getElementById('Buzz').value;
     
-    const fizznumber = Number.parseInt(fizz);
-    const buzznumber = Number.parseInt(buzz);
-    
-    for(let i = 1 ; i <= 100 ; i++){
-        if(isNaN(fizznumber && buzznumber)){
+    const fizzNumber = Number.parseInt(fizz);
+    const buzzNumber = Number.parseInt(buzz);
+
+    for(let i = 1; i <= 100; i++){
+        const li = document.createElement('li');
+        if(isNaN(fizzNumber && buzzNumber)){
             li.textContent = `整数値を入力してください`;
         }else{
-            if(i % fizznumber === 0 && i % buzznumber === 0){
+            if(i % fizzNumber === 0 && i % buzzNumber === 0){
                 li.textContent = `fizzbuzz${i}`;
-            }else if(i % fizznumber === 0){
+            }else if(i % fizzNumber === 0){
                 li.textContent = `fizz${i}`;
-            }else if(i % buzznumber === 0){
+            }else if(i % buzzNumber === 0){
                 li.textContent = `buzz${i}`;
             }
         }
