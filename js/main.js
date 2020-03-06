a7b3e6e2feb1171370c34489bd39f39a39d4d714
Daiki <daiki.@DaikinoAir.local>
2020-03-06 23:39:02 +0900

    add file goodmorning

--- js/main.js
+++ js/main.js
@@ -3,25 +3,33 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click' , () => {
 
+    const lists = document.getElementById('lists');
+
+    if(!(lists === '')) {
+        lists.innerHTML = '';
+    }
+
     const fizz = document.getElementById('Fizz').value;
     const buzz = document.getElementById('Buzz').value;
     
     const fizzNumber = Number.parseInt(fizz);
     const buzzNumber = Number.parseInt(buzz);
 
-    for(let i = 1; i <= 100; i++){
-        const li = document.createElement('li');
-        if(isNaN(fizzNumber && buzzNumber)){
-            li.textContent = `整数値を入力してください`;
-        }else{
+    const li = document.createElement('li');
+    if(isNaN(fizzNumber && buzzNumber)){
+        li.textContent = `整数値を入力してください`;
+        lists.appendChild(li);
+    }else{
+        for(let i = 1; i <= 100; i++){
+            const li = document.createElement('li');  
             if(i % fizzNumber === 0 && i % buzzNumber === 0){
-                li.textContent = `fizzbuzz${i}`;
-            }else if(i % fizzNumber === 0){
-                li.textContent = `fizz${i}`;
-            }else if(i % buzzNumber === 0){
-                li.textContent = `buzz${i}`;
-            }
+                    li.textContent = `fizzbuzz${i}`;
+                }else if(i % fizzNumber === 0){
+                    li.textContent = `fizz${i}`;
+                }else if(i % buzzNumber === 0){
+                    li.textContent = `buzz${i}`;
+                }
+        lists.appendChild(li);
         }
-        document.getElementById('lists').appendChild(li);
     }
 });
